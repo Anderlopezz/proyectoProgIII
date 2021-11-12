99c71323bd303d277af3eaec838bfa77366039b6
Anderlopezz <ander.lopez27@opendeusto.es>
2021-11-12 18:51:02 +0100

    Anyadido utils

---
+++ src/utils/utils.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.ArrayList;
+
+import clases.Cliente;
+public class utils 
+{
+	public static int sumarEdades(Cliente c, Cliente c1){
+		return c.getEdad()+c1.getEdad();
+	}
+	
+}
