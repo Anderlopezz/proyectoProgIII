40e8cf2d5a2438385b11cc66f0bc4eaa44d9a6e8
Anderlopezz <ander.lopez27@opendeusto.es>
2021-12-01 21:47:32 +0100

    Añadido nuevo Login1

--- src/ventanas/VentanaAdmin.java
+++ src/ventanas/VentanaAdmin.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class VentanaAdmin extends JFrame {
 
@@ -32,11 +33,23 @@ public class VentanaAdmin extends JFrame {
 	 */
 	public VentanaAdmin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 848, 545);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(311, 202, 164, 48);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(311, 277, 164, 48);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(311, 357, 164, 48);
+		contentPane.add(btnNewButton_2);
 	}
 
 }
