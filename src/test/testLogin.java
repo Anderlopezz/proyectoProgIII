2b3da3e53157f5bfe4ee4c6f42d5e22b4c4b7fa9
Anderlopezz <ander.lopez27@opendeusto.es>
2021-12-02 21:18:48 +0100

    Test Login first try

---
+++ src/test/testLogin.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testLogin {
+	
+
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		//String admin = admin;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	
+	}
+
+	@Test
+	public void test() {
+		
+		try{
+			//Usuario.Contrasena();
+			fail();
+			
+		}catch(Exception e){	
+		}	
+	}
+
+}
